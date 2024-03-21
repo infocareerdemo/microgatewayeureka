222109d19a5730a9f945cbd6dcf6518f1f72d5c4
LakkaiyaRK <ramkumar.lakkaiya@thoughtclan.com>
2024-03-21 14:46:57 +0530

    external call from user to product

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -3,6 +3,9 @@ package com.example.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,9 @@ public class UserApplication {
 		SpringApplication.run(UserApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced //If loadbalanced annotation is used, then you won't be able to call Api using localhost
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- user/src/main/java/com/example/user/controller/UserController.java
+++ user/src/main/java/com/example/user/controller/UserController.java
@@ -1,15 +1,27 @@
 package com.example.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@GetMapping("/name")
 	public String getUser() {
 		return "This is user service...!";
+	}
+	
+	@GetMapping("/external")
+	public String callNestedSevice() {
+		String uri = "http://product/product/getProduct";
+		String data = restTemplate.getForObject(uri, String.class);
+		return "External Call Response : " + data;
 	}
 }
